790706c08d0cbd49fa201796480fc9a0c8a74786
harimill <144196982+harimill@users.noreply.github.com>
2025-04-21 17:59:14 -0700

    Update api_service.dart

--- uwbprivacyapp/lib/services/api_service.dart
+++ uwbprivacyapp/lib/services/api_service.dart
@@ -6,7 +6,7 @@ import '../models/sensor.dart';
 import 'uwb_service.dart';
 
 class APIService {
-  static const String baseUrl = 'https://your-backend-api.com';
+  static const String baseUrl = 'https://ser517group.onrender.com/api/v1/beacons';
 
   final BuildContext context;
 
